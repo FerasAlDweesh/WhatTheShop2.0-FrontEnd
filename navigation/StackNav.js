cad9cb071507879c89dfb95de66c03e8c3393753
barak <balmeshaan@gmail.com>
2019-12-10 17:30:51 +0300

    removed bottomNav and replaced with icons in header

--- navigation/StackNav.js
+++ navigation/StackNav.js
@@ -3,11 +3,19 @@ import { createStackNavigator } from "react-navigation-stack";
 // Components
 import List from "../components/List";
 import Detail from "../components/Detail";
+import Profile from "../components/Profile";
+import Login from "../components/Login";
+import Register from "../components/Register";
+import Cart from "../components/Cart";
 
 const StackNav = createStackNavigator(
   {
     List: List,
-    DetailScreen: Detail
+    DetailScreen: Detail,
+    Profile: Profile,
+    Login: Login,
+    Register: Register,
+    Cart: Cart
   },
   {
     initialRouteName: "List",
